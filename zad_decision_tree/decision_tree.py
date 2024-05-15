3ca1870818cd3e7c55c10d816bb15e04fa3cb9d8
Jakub Puszka <126493564+PuszkaJakub@users.noreply.github.com>
2024-05-15 12:38:54 +0200

    Add files via upload
    final files

--- zad_decision_tree/decision_tree.py
+++ zad_decision_tree/decision_tree.py
@@ -2,84 +2,100 @@ import numpy as np
 from collections import Counter
 
 class Node:
-    def __init__(self, feature=None, threshold=None, left=None, right=None,*,value=None):
+    def __init__(self, feature=None, threshold=None, left=None, right=None, *, value=None):
         self.feature = feature
         self.threshold = threshold
         self.left = left
         self.right = right
         self.value = value
 
-        def is_leaf_node(self):
-            return self.value is not None
+    def is_leaf_node(self):
+        return self.value is not None
 
 class DecisionTree:
-    def __init__(self, min_samples_split=2, max_depth=100, n_features=None):
+    def __init__(self, min_samples_split=2, max_depth=10, n_features=3):
         self.min_samples_split = min_samples_split
         self.max_depth = max_depth
         self.n_features = n_features
         self.root = None
 
+    def fit(self, X, y):
+        self.root = self._grow_tree(X, y, 0)
 
-        def fit(self, X, y):
-            self.root = self._grow_tree(X, y, 0)
+    def _grow_tree(self, X, y, depth=0):
+        # ilosc probek, ilosc cech
+        num_of_samples, num_of_features = np.shape(X)
 
-        def _grow_tree(self, X, y, depth=0):
-            # ilosc probek, ilosc cech
-            num_of_samples, num_of_features = np.shape(X)
+        # Jesli warunek stopu jest spelniony, wezel jest lisciem
+        if num_of_samples <= self.min_samples_split or depth > self.max_depth:
+            # znajduje i przypisuje jako wartosc liscia najczesciej wystepujacy typ
+            counter = Counter(y)
+            most_common = counter.most_common(1)
+            if most_common:
+                value = most_common[0][0]
+            else:
+                value = None
+            return Node(value= value)
 
-            # Jesli warunek stopu jest spelniony, wezel jest lisciem
-            if num_of_samples <= self.min_samples_split or depth > self.max_depth:
-                # znajduje i przypisuje jako wartosc liscia najczesciej wystepujacy typ
-                counter = Counter(y)
-                most_common = counter.most_common(1)[0][0]
-                return Node(value=most_common)
+        # Jesli nie jest spelniony, to jest to wezel decyzyjny
 
-            # Jesli nie jest spelniony, to jest to wezel decyzyjny
+        # losowanie probki do podzialu
+        feat_idxs = np.random.choice(num_of_features, self.n_features, replace=False)
+        # szukanie najlepszego podzialu, zwraca dane nt. najlepszej cechy i jej wartosci progowej
+        best_feature, best_threshold = self._best_split(X, y, feat_idxs)
+        # podzial
+        left_features, right_features = self._split(X[:, best_feature], y, best_threshold)
+        # stworzenie dzieci wezla
+        left_subtree = self._grow_tree(X[left_features, :], y[left_features], depth + 1)
+        right_subtree = self._grow_tree(X[right_features, :], y[right_features], depth + 1)
+        # zwrocenie kompletnego wezla
+        return Node(feature=best_feature, threshold=best_threshold, left=left_subtree, right=right_subtree)
 
-            # losowanie probki do podzialu
-            feat_idxs = np.random.choice(num_of_features, self.n_features, replace=False)
-            # szukanie najlepszego podzialu, zwraca dane nt. najlepszej cechy i jej wartosci progowej
-            best_feature, best_threshold = self._best_split(X, y, feat_idxs)
-            # podzial
-            left_features, right_features = self._split(X[:, best_feature], y, best_threshold)
-            # stworzenie dzieci wezla
-            left_subtree = self._grow_tree(X[left_features, :], y[left_features], depth+1)
-            right_subtree = self._grow_tree(X[right_features, :], y[right_features], depth+1)
-            # zwrocenie kompletnego wezla
-            return Node(feature=best_feature, threshold=best_threshold, left=left_subtree, right=right_subtree)
+    def _best_split(self, X, y, feat_idxs):
+        best_feature, best_threshold = None, None
+        best_info_gain = float('-inf')
+        # iteruje przez wszystkie cechy
+        for feat_idx in feat_idxs:
+            # iteruje przez wszystkie wartosci cech
+            thresholds = np.unique(X[:, feat_idx])
+            for threshold in thresholds:
+                # jesli znajdzie lepsze info_gain to zamienia
+                gain = self._information_gain(X[:, feat_idx], y, threshold)
+                if gain > best_info_gain:
+                    best_info_gain = gain
+                    best_feature = feat_idx
+                    best_threshold = threshold
+        return best_feature, best_threshold
 
-        def _best_split(self, X, y, feat_idxs):
-            best_feature, best_threshold = None, None
-            best_info_gain = float('-inf')
-            # iteruje przez wszystkie cechy
-            for feat_idx in feat_idxs:
-                # iteruje przez wszystkie wartosci cech
-                thresholds = np.unique(X[:, feat_idx])
-                for threshold in thresholds:
-                    # jesli znajdzie lepsze info_gain to zamienia
-                    gain = self._information_gain(X[:, feat_idx], y, threshold)
-                    if gain > best_info_gain:
-                        best_info_gain = gain
-                        best_feature = feat_idx
-                        best_threshold = threshold
-            return best_feature, best_threshold
+    def _information_gain(self, X, y, threshold):
+        # obliczanie information gain wg wzoru
+        parent_entropy = self._entropy(y)
+        left_idxs, right_idxs = self._split(X, y, threshold)
+        if len(left_idxs) == 0 or len(right_idxs) == 0:
+            return 0
+        n = len(y)
+        n_l, n_r = len(left_idxs), len(right_idxs)
+        left_entropy = self._entropy(y[left_idxs])
+        right_entropy = self._entropy(y[right_idxs])
+        child_entropy = (n_l / n) * left_entropy + (n_r / n) * right_entropy
+        return parent_entropy - child_entropy
 
+    def _split(self, X, y, split_thresh):
+        left_features = np.where(X <= split_thresh)[0]
+        right_features = np.where(X > split_thresh)[0]
+        return left_features, right_features
 
-        def _information_gain(self, X, y, threshold):
-            pass
+    def _entropy(self, y):
+        hist = np.bincount(y)
+        ps = hist / len(y)
+        return -np.sum([p * np.log(p) for p in ps if p > 0])
 
-        def _split(self, X, y, split_thresh):
-            left_features = np.where(X <= split_thresh)[0]
-            right_features = np.where(X > split_thresh)[0]
-            return left_features, right_features
+    def _traverse_tree(self, x, node):
+        if node.is_leaf_node():
+            return node.value
+        if x[node.feature] < node.threshold:
+            return self._traverse_tree(x, node.left)
+        return self._traverse_tree(x, node.right)
 
-        def _entropy(self, y):
-            hist = np.bincount(y)
-            ps = hist / len(y)
-            return -np.sum([p * np.log(p) for p in ps if p>0])
-
-        def _traverse_tree(self, x, node):
-            pass
-
-        def predict(self, X):
-            return np.array([self._traverse_tree(x, self.root) for x in X])
+    def predict(self, X):
+        return np.array([self._traverse_tree(x, self.root) for x in X])
--- zad_decision_tree/main.py
+++ zad_decision_tree/main.py
@@ -8,16 +8,13 @@ X, y = data.data, data.target
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=1234)
 
-print(X_train)
-print(X_train.shape)
 
+clf = DecisionTree()
+clf.fit(X_train, y_train)
+predictions = clf.predict(X_test)
 
-# clf = DecisionTree()
-# clf.fit(X_train, y_train)
-# predictions = clf.predict(X_test)
-#
-# def accuracy(y_true, y_pred):
-#     return np.sum(y_test == y_pred) / len(y_test)
-#
-# acc = accuracy(y_test, predictions)
-# print(acc)
+def accuracy(y_true, y_pred):
+    return np.sum(y_test == y_pred) / len(y_test)
+
+acc = accuracy(y_test, predictions)
+print(acc)
