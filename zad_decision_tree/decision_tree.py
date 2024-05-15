b0cf1b632a1460685438a50b4c43bd7c548e7727
Jakub Puszka <126493564+PuszkaJakub@users.noreply.github.com>
2024-05-15 12:13:05 +0200

    Add files via upload
    fit, grow_tree, best_split and split methods

--- zad_decision_tree/decision_tree.py
+++ zad_decision_tree/decision_tree.py
@@ -21,20 +21,57 @@ class DecisionTree:
 
 
         def fit(self, X, y):
-
-            pass
+            self.root = self._grow_tree(X, y, 0)
 
         def _grow_tree(self, X, y, depth=0):
-            pass
+            # ilosc probek, ilosc cech
+            num_of_samples, num_of_features = np.shape(X)
+
+            # Jesli warunek stopu jest spelniony, wezel jest lisciem
+            if num_of_samples <= self.min_samples_split or depth > self.max_depth:
+                # znajduje i przypisuje jako wartosc liscia najczesciej wystepujacy typ
+                counter = Counter(y)
+                most_common = counter.most_common(1)[0][0]
+                return Node(value=most_common)
+
+            # Jesli nie jest spelniony, to jest to wezel decyzyjny
+
+            # losowanie probki do podzialu
+            feat_idxs = np.random.choice(num_of_features, self.n_features, replace=False)
+            # szukanie najlepszego podzialu, zwraca dane nt. najlepszej cechy i jej wartosci progowej
+            best_feature, best_threshold = self._best_split(X, y, feat_idxs)
+            # podzial
+            left_features, right_features = self._split(X[:, best_feature], y, best_threshold)
+            # stworzenie dzieci wezla
+            left_subtree = self._grow_tree(X[left_features, :], y[left_features], depth+1)
+            right_subtree = self._grow_tree(X[right_features, :], y[right_features], depth+1)
+            # zwrocenie kompletnego wezla
+            return Node(feature=best_feature, threshold=best_threshold, left=left_subtree, right=right_subtree)
 
         def _best_split(self, X, y, feat_idxs):
-            pass
+            best_feature, best_threshold = None, None
+            best_info_gain = float('-inf')
+            # iteruje przez wszystkie cechy
+            for feat_idx in feat_idxs:
+                # iteruje przez wszystkie wartosci cech
+                thresholds = np.unique(X[:, feat_idx])
+                for threshold in thresholds:
+                    # jesli znajdzie lepsze info_gain to zamienia
+                    gain = self._information_gain(X[:, feat_idx], y, threshold)
+                    if gain > best_info_gain:
+                        best_info_gain = gain
+                        best_feature = feat_idx
+                        best_threshold = threshold
+            return best_feature, best_threshold
+
 
         def _information_gain(self, X, y, threshold):
             pass
 
         def _split(self, X, y, split_thresh):
-            pass
+            left_features = np.where(X <= split_thresh)[0]
+            right_features = np.where(X > split_thresh)[0]
+            return left_features, right_features
 
         def _entropy(self, y):
             hist = np.bincount(y)
